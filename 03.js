3dc946a7682e4bc79467a473129d33858e8e904d
annpurna-jha <annpurnajha4@gmail.com>
2023-10-17 19:25:37 +0530

    Add your commit

--- 03.js
+++ 03.js
@@ -13,7 +13,7 @@ console.log(typeof (age+""));//parantheses after typeof are not required
 //convert string to number
 let mystr = "22";
 // console.log(typeof mystr);
-// mystr = +"22"; // add + symbol infront of string will convert it into number
+// mystr = +"22"; // add + or - symbol in front of string will convert it into number
 
 // mystr = Number(mystr); // // or we can do it like
 // console.log(typeof mystr); 

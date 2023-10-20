694775a9bdbe3eaebae9f6712c45f0cbdc22e0f9
annpurna-jha <annpurnajha4@gmail.com>
2023-10-20 23:19:18 +0530

    Read about Window Object, DOM, BOM

---
+++ 71.js
@@ -0,0 +1,13 @@
+// window object represents browser window and provides methods to control it. It is a global object
+console.log(window);
+// or we can do it like this
+window.console.log(window);//bcz window is a global object and every methods and everything is inside window
+
+// DOM (Document Object Model)
+// represents the page content as HTML
+document.body.style.background = "yellow"; // if we want to perfom some css on user's action
+
+// BOM(Browser Object Model)
+// represents additional objects provided by the browser (host enviroment) for working with everything except the document
+
+// alert, confirm and prompt are also a part of the BOM

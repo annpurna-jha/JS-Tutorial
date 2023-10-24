e9fdb6ae35d21c27773fbad8d6e93482e23ec427
annpurna-jha <annpurnajha4@gmail.com>
2023-10-24 10:19:20 +0530

    insert adjacent element, html, text

---
+++ Code WIth Harry/insert Adjacent/script.js
@@ -0,0 +1,10 @@
+// first.insertAdjacentHTML('beforebegin' , '<div class = "test"> beforebegin</div>');//added before starting of "#first"
+
+// first.insertAdjacentHTML('beforeend' , '<div class = "test"> beforeend</div>');//added inside "#first" just before end of  "#first" , and after the content of "#first"
+
+// first.insertAdjacentHTML('afterbegin' , '<div class = "test"> afterbegin</div>');//added inside "#first" just after , and before the content of "#first"
+
+// first.insertAdjacentHTML('afterend' , '<div class = "test"> afterend</div>');//added after end of "#first"
+
+// node.remove() //to remove a node
+first.remove();

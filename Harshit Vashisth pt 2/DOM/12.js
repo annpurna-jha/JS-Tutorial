96e3ec51b3cc9133849ad2f2c3ec8a6c98aa0e06
annpurna-jha <annpurnajha4@gmail.com>
2023-11-02 19:22:46 +0530

    classList ,add and remove, toggle classes

---
+++ Harshit Vashisth pt 2/DOM/12.js
@@ -0,0 +1,16 @@
+//  classList ,add and remove, toggle classes
+
+const sectionTodo = document.querySelector(".section-todo");
+// console.log(sectionTodo.classList);
+
+// add class using js
+// sectionTodo.classList.add('bg-dark');
+
+// remove class using js
+// sectionTodo.classList.remove("container");
+
+const ans = sectionTodo.classList.contains("container");
+console.log(ans);
+
+// sectionTodo.classList.toggle("bg-dark");//if class present remove , if not then add
+

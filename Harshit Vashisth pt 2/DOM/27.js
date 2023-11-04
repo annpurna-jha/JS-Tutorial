7ac44f21c4827629b61e61d80ff554874d7915c9
annpurna-jha <annpurnajha4@gmail.com>
2023-11-04 22:37:32 +0530

    Keypress & mouseover event

---
+++ Harshit Vashisth pt 2/DOM/27.js
@@ -0,0 +1,15 @@
+// Keypress & mouseover event
+const body = document.body;
+body.addEventListener("keypress",(e)=>{
+    // console.log(e);
+    console.log(e.key);//press any key display on console
+});
+
+// mouseover event
+const mainBtn = document.querySelector(".btn-headline");
+mainBtn.addEventListener("mouseover",()=>{
+    console.log("MouseOver event occured!!");//when mouse goes on that button
+});
+mainBtn.addEventListener("mouseleave",()=>{
+    console.log("Mouseleave event occured!!");//when mouse remove from that button
+});
